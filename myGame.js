097e62d7a7b3fac948c1b0f60cc5d9b6093b9981
UgniusStelingis <ugnius.stelingis@gmail.com>
2020-03-30 15:00:22 +0100

    last upgrade

--- myGame.js
+++ myGame.js
@@ -2,8 +2,23 @@ var colorNum = 6;
 var square = document.querySelectorAll(".square");
 var mode = document.querySelectorAll("#mode");
 var btnreset = document.getElementById("reset");
+var pickColor, pickedColor;
+var targetColor = document.getElementById("targetColor");
 
 randomColor(colorNum);
+
+targetColor.textContent = pickColor;
+
+for (var i = 0; i < square.length; i++) {
+  square[i].addEventListener("click", function() {
+    pickedColor = this.style.backgroundColor;
+    if (pickedColor === pickColor) {
+      alert("CONGRATULATIONS! You Win!");
+    } else {
+      this.style.display = "none";
+    }
+  });
+}
 
 btnreset.addEventListener("click", function() {
   randomColor(colorNum);
@@ -13,22 +28,15 @@ mode[0].addEventListener("click", function() {
   mode[0].classList.add("selected");
   mode[1].classList.remove("selected");
   colorNum = 3;
+  randomColor(colorNum);
 });
 
 mode[1].addEventListener("click", function() {
   mode[1].classList.add("selected");
   mode[0].classList.remove("selected");
   colorNum = 6;
+  randomColor(colorNum);
 });
-
-/* mode.addEventListener("click", function() {
-  alert("hi!");
-});
-
-mode.addEventListener("click", function() {
-  alert("connected!");
-});
- */
 
 function colorGenerator() {
   var r = Math.floor(Math.random() * 255 + 1);
@@ -40,7 +48,10 @@ function colorGenerator() {
 function randomColor(colorNum) {
   for (var i = 0; i < square.length; i++) {
     if (i < colorNum) {
+      square[i].style.display = "block";
       square[i].style.backgroundColor = colorGenerator();
+      pickColor =
+        square[Math.floor(Math.random() * square.length)].style.backgroundColor;
     } else {
       square[i].style.display = "none";
     }
